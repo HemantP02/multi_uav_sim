02f4d419fa5eab0e47f86e2ab780535c596c8457
Hemant Pandey <124810620+HemantP02@users.noreply.github.com>
2024-12-24 15:08:30 +0530

    Update setpoint_node_t1.cpp

--- srcipts/setpoint_node_t1.cpp
+++ srcipts/setpoint_node_t1.cpp
@@ -1,1 +1,2 @@
-#This file is is supposed to give command for formation
+//This file is is supposed to give command for formation I have't included it but if required you can contanct me other files are sufficient if,
+//a bit of thinking is done
